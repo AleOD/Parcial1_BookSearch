6b3a84cdd56bb55ac5125822d8752d269d031553
Asiel Trejo <AsielTrejo@Asiels-MacBook-Pro-2.local>
2021-09-13 09:42:58 -0600

    search function in console

--- search.js
+++ search.js
@@ -21,13 +21,14 @@ function searchBook(){
      });
 
   fetch(request)
-  .then(function(data) {   // si hay exito
+  .then(function(res) {   // si hay exito
      console.log('data');
      console.log(data);
      data.json()
      .then(function(myjson) {
        console.log('results =');
        console.log(myjson);
+       printResults();
      });
 
   })
@@ -37,6 +38,43 @@ function searchBook(){
 	}
 }
 
-function printError(){
+//function printError(){
 	
+//}
+function printResults(){
+  for (var i = 0; i<res.itesm.length; i+=3) {
+    item.res.items[i];
+    title1 = item.volumeInfo.title;
+    author1 = item.volumeInfo.author;
+    publisher1=item.volumeInfo.publisher;
+    bookLink1=item.volumeInfo.previewLink;
+
+    item.res.items[i+1];
+    title2 = item.volumeInfo.title;
+    author2 = item.volumeInfo.author;
+    publisher2=item.volumeInfo.publisher;
+    bookLink2=item.volumeInfo.previewLink;
+
+    item.res.items[i+2];
+    title3 = item.volumeInfo.title;
+    author3 = item.volumeInfo.author;
+    publisher3 =item.volumeInfo.publisher;
+    bookLink3 =item.volumeInfo.previewLink;
+
+  outputList.innerHTML += '<div class = "row mt-4'> + formatPrint(title1,author1,publisher1,bookLink1) + formatPrint(title2,author2,publisher2,bookLink2) + formatPrint(title3,author3,publisher3,bookLink3);
+
+    //image
+
 }
+}
+
+// function formatPrint(title,author,publisher,bookLink){
+//   var htmlCard = `
+//   <div class = "colg-lg-6> 
+//   <div class = "row no-gutters">
+//     <div class = "col-md-4"
+
+//   `
+
+//   return htmlCard;
+// }

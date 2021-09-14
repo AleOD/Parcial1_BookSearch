8bf4d576c93ce140e648e0d1675345f5b78c06ba
Asiel Trejo <AsielTrejo@Asiels-MacBook-Pro-2.local>
2021-09-14 17:50:20 -0600

    Code done until now #8

--- search.js
+++ search.js
@@ -1,80 +1,92 @@
-var item, title, author, publisher, link, img
-var displayList = ""
-var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
+var item, title, author, publisher, link, img;
+var displayList = document.getElementById("results");
+var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=search+";
 //var placeHldr = '<img src = '
 var searchTerm;
 var data;
 
 function searchBook(){
-	displayList = document.getElementById("output")
-	displayList.innerHTML = ""
-	searchTerm =   document.getElementById("search");
-	if(searchTerm === "" || searchTerm === null){
-		printError();
-	}
+  
+  displayList.innerHTML = " ";
+  searchTerm =  document.getElementById("search").value;
 
-	else{
-		  var request = new Request(bookUrl + searchTerm,
+  if(searchTerm === "" || searchTerm === null){
+    //printError();
+  }
+
+  else{
+      var request = new Request(bookUrl + searchTerm,
      {
        method : 'GET',
-       headers: new Headers({ 'Content-Type': 'application/json'})
+
      });
 
   fetch(request)
   .then(function(res) {   // si hay exito
-     console.log('data');
-     console.log(data);
-     data.json()
+     res.json()
      .then(function(myjson) {
-       console.log('results =');
-       console.log(myjson);
-       printResults();
-     });
-
+      // console.log("el resultado del error es");
+      // console.log(myjson);
+      // console.log("Estos fueron todos los encontrados");
+      // console.log(myjson.totalItems);
+      if(myjson.totalItems>0){
+       printResults(myjson);        
+      }
+      else{
+        //printError();
+      }
+     } 
+     );     
   })
   .catch(err => {  // en caso de error
      console.log(err);
-  });
-	}
+     printError();
+
+  } );
+  
+  }
 }
 
-//function printError(){
-	
-//}
-function printResults(){
-  for (var i = 0; i<res.itesm.length; i+=3) {
-    item.res.items[i];
+function printError(){
+  displayList.innerHTML = "Error : no books were found";
+  console.log("Error");
+}
+function printResults(res){
+
+
+  for (var i = 0; i < res.items.length; i+=1) {
+    item = res.items[i];
     title1 = item.volumeInfo.title;
-    author1 = item.volumeInfo.author;
+    author1 = item.volumeInfo.authors;
     publisher1=item.volumeInfo.publisher;
     bookLink1=item.volumeInfo.previewLink;
 
-    item.res.items[i+1];
-    title2 = item.volumeInfo.title;
-    author2 = item.volumeInfo.author;
-    publisher2=item.volumeInfo.publisher;
-    bookLink2=item.volumeInfo.previewLink;
 
-    item.res.items[i+2];
-    title3 = item.volumeInfo.title;
-    author3 = item.volumeInfo.author;
-    publisher3 =item.volumeInfo.publisher;
-    bookLink3 =item.volumeInfo.previewLink;
+  document.getElementById("results").innerHTML += '<div">' + formatPrint(title1,author1,publisher1,bookLink1) + '</div>';
 
-  outputList.innerHTML += '<div class = "row mt-4'> + formatPrint(title1,author1,publisher1,bookLink1) + formatPrint(title2,author2,publisher2,bookLink2) + formatPrint(title3,author3,publisher3,bookLink3);
 
-    //image
+    //image missing
 
+  }
+  console.log("Result");
+  console.log(displayList.innerHTML);
 }
+/*
+* @param title1 author1 publisher1 bookLink1
+* @return htmlCard
+*/
+
+
+function formatPrint(titles,authors,publishers,bookLinks){
+  var htmlCard =
+  `
+          <div>
+            <h5 class =""> ${titles} </h5>
+            <p class =""> Author: ${authors} </p>
+            <p class =""> Publisher: ${publishers} </p>
+            <p class =""> link: ${bookLinks} </p>
+          </div>
+
+  `
+  return htmlCard;
 }
-
-// function formatPrint(title,author,publisher,bookLink){
-//   var htmlCard = `
-//   <div class = "colg-lg-6> 
-//   <div class = "row no-gutters">
-//     <div class = "col-md-4"
-
-//   `
-
-//   return htmlCard;
-// }

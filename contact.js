f167a70dda669da29d78f9f6e0885ea34c4dc180
Asiel Trejo <AsielTrejo@Asiels-MacBook-Pro-2.local>
2021-09-14 21:29:59 -0600

    Download contact in a txt #21

---
+++ contact.js
@@ -0,0 +1,36 @@
+var contact = {
+  nombre: "",
+  mensaje: "",
+  email: ""
+};
+
+var contactJson;
+
+
+
+function grabContact() {
+  var nombreElement = document.getElementById("nombre");
+  var mensajeElement = document.getElementById("mensaje");
+  var emailElement = document.getElementById("email");
+
+  contact.nombre = nombreElement.value;
+  contact.mensaje = mensajeElement.value;
+  contact.email = emailElement.value;
+
+  //console.log(contact);
+  contactJson = JSON.stringify(contact);
+  console.log(contactJson);
+
+const a = document.createElement("a");
+  a.href = URL.createObjectURL(new Blob([contactJson], {
+    type: "text/plain"
+  }));
+  a.setAttribute("download", "data.txt");
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+}
+
+
+
